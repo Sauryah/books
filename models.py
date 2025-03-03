4c13eb9aee054a951f37418ec928e999aca97e8c
Sahil Pradhan <164321562+Sauryah@users.noreply.github.com>
2025-03-03 07:47:56 -0800

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,18 @@
+from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    publish_date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    distribution_expense = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
